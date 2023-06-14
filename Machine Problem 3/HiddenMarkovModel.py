52cabd3674997499e5bc0fc1ec9d9ffb67990534
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-06-15 01:05:08 +0900

    i dunno what im doing lol

--- Machine Problem 3/HiddenMarkovModel.py
+++ Machine Problem 3/HiddenMarkovModel.py
@@ -1,8 +1,10 @@
 """
-Machine Problem 2 - Hidden Markov Model
+Machine Problem 3 - Hidden Markov Model
 
 References:
     https://towardsdatascience.com/hidden-markov-model-implemented-from-scratch-72865bda430e?gi=5d1e73d86e55
+    https://medium.com/@kangeugine/hidden-markov-model-7681c22f5b9
+    http://www.infocobuild.com/education/audio-video-courses/computer-science/cs188-spring2015-berkeley.html
 
     T: length of the observation sequence.
     N: number of latent (hidden) states.
@@ -16,3 +18,15 @@ References:
     X: (x₀, x₁, …), x_t ∈ Q - hidden state sequence.
 
 """
+
+
+class HMM:
+    def __init__(self, Q: set, V: set, A: list[list], B: list[list], π: list):
+
+        self.N = len(Q)
+        self.M = len(V)
+        self.Q = Q
+        self.V = V
+        self.A = A
+        self.B = B
+        self.π = π
