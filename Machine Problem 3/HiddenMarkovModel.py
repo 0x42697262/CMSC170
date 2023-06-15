8624005296f12a33ffafc0249c687d8171dc5de8
KrulYuno <giogulane.wineffgulane@gmail.com>
2023-06-15 19:57:43 +0900

    forward algorithm, but we should not do this. since too advanced

--- Machine Problem 3/HiddenMarkovModel.py
+++ Machine Problem 3/HiddenMarkovModel.py
@@ -1,36 +1,41 @@
-"""
-Machine Problem 3 - Hidden Markov Model
+import numpy as np
 
-References:
-    https://towardsdatascience.com/hidden-markov-model-implemented-from-scratch-72865bda430e?gi=5d1e73d86e55
-    https://medium.com/@kangeugine/hidden-markov-model-7681c22f5b9
-    http://www.infocobuild.com/education/audio-video-courses/computer-science/cs188-spring2015-berkeley.html
-
-    T: length of the observation sequence.
-    N: number of latent (hidden) states.
-    M: number of observables.
-    Q: {q₀, q₁, …} - hidden states.
-    V: {0, 1, …, M — 1} - set of possible observations.
-    A: state transition matrix.
-    B: emission probability matrix.
-    π: initial state probability distribution.
-    O: observation sequence.
-    X: (x₀, x₁, …), x_t ∈ Q - hidden state sequence.
-
-"""
-
-
-Q = {'Raininy', 'Sunny'}
-V = {'Walk', 'Shop', 'Clean'}
+Q = ['Rainy', 'Sunny']
+V = ['Walk', 'Shop', 'Clean']
 N = len(Q)
 M = len(V)
-A = [
-        [0.7, 0.3],
-        [0.4, 0.6],
-        ]
-B = [
-        [0.1, 0.4, 0.5],
-        [0.6, 0.3, 0.1],
-        ]
-π = [0.6, 0.4]
 
+A = np.array([
+    [0.7, 0.3],
+    [0.4, 0.6]
+])
+
+B = np.array([
+    [0.1, 0.4, 0.5],
+    [0.6, 0.3, 0.1]
+])
+
+π = np.array([0.6, 0.4])
+
+# Calculate the probability of the event "Walk" using the forward algorithm
+def forward_algorithm(observation_sequence):
+    T = len(observation_sequence)
+    alpha = np.zeros((T, N))
+
+    # Initialization
+    alpha[0] = π * B[:, V.index(observation_sequence[0])]
+
+    # Recursion
+    for t in range(1, T):
+        for j in range(N):
+            alpha[t, j] = np.dot(alpha[t - 1], A[:, j]) * B[j, V.index(observation_sequence[t])]
+
+    return alpha
+
+observation_sequence = ['Walk']
+alpha = forward_algorithm(observation_sequence)
+
+# Calculate the probability of the event "Walk"
+probability_walk = np.sum(alpha[-1])
+
+print("Probability of Walk:", probability_walk)
